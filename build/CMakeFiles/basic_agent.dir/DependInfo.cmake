a5774ef4efc749acdd44863bc8bcea86524f5fb9
William Nicolussi Zom <william.nicolussi@gmail.com>
2025-11-13 10:24:44 +0100

    folder build configurated

---
+++ build/CMakeFiles/basic_agent.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/HP/Desktop/UniTn/IVAD/autonomous_traffic_light-main/log/screen_print.c" "CMakeFiles/basic_agent.dir/log/screen_print.c.obj" "gcc" "CMakeFiles/basic_agent.dir/log/screen_print.c.obj.d"
+  "C:/Users/HP/Desktop/UniTn/IVAD/autonomous_traffic_light-main/matlab/codegen/lib/primitives/primitives.c" "CMakeFiles/basic_agent.dir/matlab/codegen/lib/primitives/primitives.c.obj" "gcc" "CMakeFiles/basic_agent.dir/matlab/codegen/lib/primitives/primitives.c.obj.d"
+  "C:/Users/HP/Desktop/UniTn/IVAD/autonomous_traffic_light-main/log/logvars.cc" "CMakeFiles/basic_agent.dir/log/logvars.cc.obj" "gcc" "CMakeFiles/basic_agent.dir/log/logvars.cc.obj.d"
+  "C:/Users/HP/Desktop/UniTn/IVAD/autonomous_traffic_light-main/starting_point.cc" "CMakeFiles/basic_agent.dir/starting_point.cc.obj" "gcc" "CMakeFiles/basic_agent.dir/starting_point.cc.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
