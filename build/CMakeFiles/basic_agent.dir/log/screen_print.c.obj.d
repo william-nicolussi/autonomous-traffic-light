ee4f0fddaa335dd261936ff17a61004cabb18300
William Nicolussi Zom <william.nicolussi@gmail.com>
2025-11-13 10:24:44 +0100

    folder build configurated

---
+++ build/CMakeFiles/basic_agent.dir/log/screen_print.c.obj.d
@@ -0,0 +1,22 @@
+CMakeFiles/basic_agent.dir/log/screen_print.c.obj: \
+ C:\Users\HP\Desktop\UniTn\IVAD\autonomous_traffic_light-main\log\screen_print.c \
+ C:/ProgramData/mingw64/mingw64/x86_64-w64-mingw32/include/stdio.h \
+ C:/ProgramData/mingw64/mingw64/x86_64-w64-mingw32/include/corecrt_stdio_config.h \
+ C:/ProgramData/mingw64/mingw64/x86_64-w64-mingw32/include/corecrt.h \
+ C:/ProgramData/mingw64/mingw64/x86_64-w64-mingw32/include/_mingw.h \
+ C:/ProgramData/mingw64/mingw64/x86_64-w64-mingw32/include/_mingw_mac.h \
+ C:/ProgramData/mingw64/mingw64/x86_64-w64-mingw32/include/_mingw_secapi.h \
+ C:/ProgramData/mingw64/mingw64/x86_64-w64-mingw32/include/vadefs.h \
+ C:/ProgramData/mingw64/mingw64/x86_64-w64-mingw32/include/sdks/_mingw_ddk.h \
+ C:/ProgramData/mingw64/mingw64/x86_64-w64-mingw32/include/_mingw_off_t.h \
+ C:/ProgramData/mingw64/mingw64/x86_64-w64-mingw32/include/swprintf.inl \
+ C:/ProgramData/mingw64/mingw64/x86_64-w64-mingw32/include/sec_api/stdio_s.h \
+ C:/ProgramData/mingw64/mingw64/x86_64-w64-mingw32/include/string.h \
+ C:/ProgramData/mingw64/mingw64/x86_64-w64-mingw32/include/crtdefs.h \
+ C:/ProgramData/mingw64/mingw64/x86_64-w64-mingw32/include/sec_api/string_s.h \
+ C:/ProgramData/mingw64/mingw64/x86_64-w64-mingw32/include/time.h \
+ C:/ProgramData/mingw64/mingw64/x86_64-w64-mingw32/include/sys/timeb.h \
+ C:/ProgramData/mingw64/mingw64/x86_64-w64-mingw32/include/sec_api/sys/timeb_s.h \
+ C:/ProgramData/mingw64/mingw64/x86_64-w64-mingw32/include/_timeval.h \
+ C:/ProgramData/mingw64/mingw64/x86_64-w64-mingw32/include/pthread_time.h \
+ C:\Users\HP\Desktop\UniTn\IVAD\autonomous_traffic_light-main\log\screen_print_c.h
