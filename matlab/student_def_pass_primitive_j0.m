b8bba595624489d9585235aa43e6bba9534a9118
William Nicolussi Zom <william.nicolussi@gmail.com>
2025-11-26 12:22:31 +0100

    function pass j0 ok

--- matlab/student_def_pass_primitive_j0.m
+++ matlab/student_def_pass_primitive_j0.m
@@ -4,16 +4,33 @@
 %               2025
 %
 %
-%% Pass primitive with j0 = 0 %%
+%%
+clear; clc; close all
 
-%% Determine the optimal final velocity to pass function of j0 = 0 %%
-% final_opt_vel_j0_pass_var = ...
-% final_opt_vel_j0_pass_fun = ...
-% - Use functions 'solve', 'diff', 'subs', and 'matlabFunction'
+%% Define the folder to put the matlabfunctions
+nameExportFolder = 'functions_exported';
+currentFolder = pwd;
+exportFolder = fullfile(currentFolder, nameExportFolder); %...\matlab\def_functions
+if ~exist(exportFolder, 'dir')
+    mkdir(exportFolder);
+end
+addpath(exportFolder);
 
-%% Determine the optimal time to reach to final velocity with j0 = 0 %%
-% final_opt_time_j0_pass_var = ...
-% final_opt_time_j0_pass_fun = ...
-% - Use the function solve 
-%   use the function final_opt_time_pass_fun
-%   Use 'subs' function to solve the system using the frequency instead of the time
+%% Determine the optimal final velocity to pass function of j0=0
+syms v0 vf a0 sf tf;
+opt_pos_eq = j_opt(0, v0, a0, sf, vf, 0, tf) == 0;
+vf_j0_opt = simplify(solve(opt_pos_eq, vf));
+
+%% Export the solution
+final_opt_vel_j0_pass_fun = matlabFunction(vf_j0_opt,'Vars',[v0, a0, sf, tf],'File', fullfile(exportFolder,'final_opt_vel_j0.m'));
+
+%% Determine the optimal time to reach to final velocity with j0=0
+opt_time_eq = final_opt_time_pass(v0, a0, sf, final_opt_vel_j0(v0, a0, sf, tf)) == tf;
+tf_j0_opt_all = solve(opt_time_eq, tf);
+tf_j0_opt_1 = tf_j0_opt_all(1); % the one with '-'
+tf_j0_opt_2 = tf_j0_opt_all(2); % the one with '+'
+
+%% Export the solutions
+final_opt_time_j0_pass_fun = matlabFunction(tf_j0_opt_all,'Vars',[v0, a0, sf],'File', fullfile(exportFolder,'final_opt_time_j0.m'));
+final_opt_time_j0_pass_1_fun = matlabFunction(tf_j0_opt_1,'Vars',[v0, a0, sf],'File', fullfile(exportFolder,'final_opt_time_j0_1.m'));
+final_opt_time_j0_pass_2_fun = matlabFunction(tf_j0_opt_2,'Vars',[v0, a0, sf],'File', fullfile(exportFolder,'final_opt_time_j0_2.m'));
--- matlab/student_pass_primitive_j0.m
+++ matlab/student_pass_primitive_j0.m
@@ -6,7 +6,18 @@
 %
 %
 %%
-
-function [coefsj0, vfj0, tfj0] = pass_primitive_j0( v0, a0, sf, vfmin, vfmax)
-% ...
+function [coeffs_j0, sf_j0, tf_j0] = student_pass_primitive_j0( v0, a0, sf, vf_min, vf_max)
+    T1 = final_opt_time_j0_1(v0, a0, sf);
+    v1 = final_opt_vel_j0(v0, a0, sf, T1);
+    if vf_min<v1<vf_max
+        coeffs_j0 = coef_list(v0, a0, sf, v1, 0., T1);
+    else
+        T2 = final_opt_time_j0_2(v0, a0, sf);
+        v2 = final_opt_vel_j0(v0, a0, sf, T2);
+        if vf_min<v2<vf_max
+            coeffs_j0 = coef_list(v0, a0, sf, v2, 0., T2);
+        else
+            coeffs_j0 = [0., 0., 0., 0., 0., 0.];
+        end
+    end
 end
