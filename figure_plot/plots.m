913cb421ff4fe91ff33b599d6e8e5bb4f9ee4e88
William Nicolussi Zom <william.nicolussi@gmail.com>
2025-11-27 12:48:56 +0100

    plot update

--- figure_plot/plots.m
+++ figure_plot/plots.m
@@ -40,7 +40,7 @@ legend("a\_req","a\_real","Location","best");
 grid on;
 
 %% Plot: v(s), a(s)
-fileName = 'Test_3.csv';
+fileName = 'Test_4_Coeff.csv';
 filePath = fullfile(csvPath, fileName);
 dataCSV = readtable(filePath, 'Delimiter', {',',';'});
 sgtitle(fileName, 'Interpreter', 'None');
@@ -63,37 +63,79 @@ plot(abscissa, dataCSV.a_real);
 ylabel("Acceleration");
 legend("a\_req","a\_real","Location","best");
 grid on;
-%% Plot: v(t)
+%% Plot: v(t) with primitives
+
 fileName = 'Test_4_Coeff.csv';
 filePath = fullfile(csvPath, fileName);
 dataCSV = readtable(filePath, 'Delimiter', {',',';'});
 
-figure(2);
-abscissa = dataCSV.dist;
+cycle_first = 1; % start to plot from this cycle
+cycle_step = 10; % plot every cycle
+cycle_last =  max(dataCSV.cycle); % plot until this cycle
 
-title(fileName, 'Interpreter', 'None');
-plot(abscissa, dataCSV.v_req); hold on;
-plot(abscissa, dataCSV.v_real);
-ylabel("Velocity");
-legend("v\_req","v\_real","Location","best");
-grid on;
+figure; hold on;
 
-%% Plot: Only Acceleration
-fileName = 'Values_PI.csv';
+% plot v_req; v_real
 abscissa = dataCSV.time;
+title(fileName, 'Interpreter', 'None');
+plot(abscissa, dataCSV.v_req);
+plot(abscissa, dataCSV.v_real);
 
+for cycle = cycle_first : cycle_step : cycle_last
+    t_init = dataCSV.time(cycle);
+    t_fin = dataCSV.final_time(cycle);
+    
+    % select only the time in [t_init, t_fin]
+    idx = (dataCSV.time >= t_init) & (dataCSV.time <= t_fin);
+    t_vec = dataCSV.time(idx);
+    
+    coef_all = [ dataCSV.c0(cycle), dataCSV.c1(cycle), dataCSV.c2(cycle), dataCSV.c3(cycle), dataCSV.c4(cycle), dataCSV.c5(cycle)];
+    v_primitive = v_from_coeffs(t_vec,coef_all);
+    plot(t_vec, v_primitive, Color="green");
+    fprintf("cycle=%d   t_init=%.6f   t_fin=%.6f\n   c1=%.6f\n", cycle, t_init, t_fin, dataCSV.c1(cycle));
+end
+
+xlabel("Time"); ylabel("Velocity");
+legend("v\_req","v\_real","v\_primitive","Location","best");
+grid on; hold off;
+
+%% Plot: a(t) with primitives
+
+fileName = 'Test_4_Coeff.csv';
 filePath = fullfile(csvPath, fileName);
 dataCSV = readtable(filePath, 'Delimiter', {',',';'});
 
-figure(3);
-title(fileName, 'Interpreter', 'None');
-plot(abscissa, dataCSV.a_req); hold on;
-plot(abscissa, dataCSV.a_real);
-ylabel("Acceleration");
-legend("a\_req","a\_real","Location","best");
-grid on;
+cycle_first = 1; % start to plot from this cycle
+cycle_step = 10; % plot every cycle
+cycle_last =  max(dataCSV.cycle); % plot until this cycle
 
-%% Draw primitives
+figure; hold on;
+
+% plot v_req; v_real
+abscissa = dataCSV.time;
+title(fileName, 'Interpreter', 'None');
+plot(abscissa, dataCSV.a_req);
+plot(abscissa, dataCSV.a_real);
+
+for cycle = cycle_first : cycle_step : cycle_last
+    t_init = dataCSV.time(cycle);
+    t_fin = dataCSV.final_time(cycle);
+    
+    % select only the time in [t_init, t_fin]
+    idx = (dataCSV.time >= t_init) & (dataCSV.time <= t_fin);
+    t_vec = dataCSV.time(idx);
+    
+    coef_all = [ dataCSV.c0(cycle), dataCSV.c1(cycle), dataCSV.c2(cycle), dataCSV.c3(cycle), dataCSV.c4(cycle), dataCSV.c5(cycle)];
+    a_primitive = a_from_coeffs(t_vec,coef_all);
+    plot(t_vec, a_primitive, Color="green");
+    fprintf("cycle=%d   t_init=%.6f   t_fin=%.6f\n   c1=%.6f\n", cycle, t_init, t_fin, dataCSV.c1(cycle));
+end
+
+xlabel("Time"); ylabel("Acceleration");
+legend("a\_req","a\_real","a\_primitive","Location","best");
+grid on; hold off;
+
+%% Draw primitives velocity
 % requested velocity and velocity of the vehicles. Then write the
 % sto and pass primitives (in space and time).
 
@@ -105,29 +147,6 @@ grid on;
 %taking the coefficient you can plot the primitives.
 % 
 
-fileName = 'Test_4_Coeff.csv';
-filePath = fullfile(csvPath, fileName);
-dataCSV = readtable(filePath, 'Delimiter', {',',';'});
 
-% drag and drop the csv, con column delimiters: comma, semicolon.
-% poi esce in consolle uiopen('C:\Users\HP\Desktop\UniTn\IVAD\autonomous_traffic_light-main\bin\log_internal\Test_4_Coeff.csv',1)
 
-cycle_init = 9;
-t_init = dataCSV.time(cycle_init);
-t_fin = dataCSV.final_time(cycle_init);
-
-% select only the time in [t_init, t_fin]
-idx = (dataCSV.time >= t_init) & (dataCSV.time <= t_fin);
-t_vec = dataCSV.time(idx);
-
-coef_all = [ dataCSV.c0(cycle_init), dataCSV.c1(cycle_init), dataCSV.c2(cycle_init), dataCSV.c3(cycle_init), dataCSV.c4(cycle_init), dataCSV.c5(cycle_init)];
-coeffs = coef_all(2:6);
-v_primitive = v_from_coeffs(t_vec,coef_all);
-
-abscissa = dataCSV.time;
-title(fileName, 'Interpreter', 'None');
-plot(abscissa, dataCSV.v_req); hold on;
-plot(abscissa, dataCSV.v_real); hold on;
-plot(t_vec, v_primitive, Color="green");
-legend("v\_req","v\_real","v\_primitive","Location","best");
-grid on;
+%% Draw primitives acceleration
