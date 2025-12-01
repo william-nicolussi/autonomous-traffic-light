ec0442ed8a1705dfd7e4bb9c120988c49fed3320
William Nicolussi Zom <william.nicolussi@gmail.com>
2025-12-01 19:37:35 +0100

    Pass Primitive j0 OK

--- matlab/pass_primitive_j0.m
+++ matlab/pass_primitive_j0.m
@@ -1,23 +1,29 @@
 %#codegen
-%%
+%%vf_min
 %          Agent Logic
 %         Pass Primitive J0
 %              2025
 %
 %
 %%
-function [coeffs_j0, sf_j0, tf_j0] = student_pass_primitive_j0( v0, a0, sf, vf_min, vf_max)
+function [m, sf_j0, tf_j0] = pass_primitive_j0(v0, a0, sf, v_min, v_max)
     T1 = final_opt_time_j0_1(v0, a0, sf);
     v1 = final_opt_vel_j0(v0, a0, sf, T1);
-    if vf_min<v1<vf_max
-        coeffs_j0 = coef_list(v0, a0, sf, v1, 0., T1);
+    if (v_min<v1) && (v1<v_max)
+        tf_j0 = T1;
+        sf_j0 = v1; % (?)
+        m = coef_list(v0, a0, sf, v1, 0., T1);
     else
         T2 = final_opt_time_j0_2(v0, a0, sf);
         v2 = final_opt_vel_j0(v0, a0, sf, T2);
-        if vf_min<v2<vf_max
-            coeffs_j0 = coef_list(v0, a0, sf, v2, 0., T2);
+        if (v_min<v2) && (v2<v_max)
+            m = coef_list(v0, a0, sf, v2, 0., T2);
+            sf_j0 = v2;
+            tf_j0 = T2;
         else
-            coeffs_j0 = [0., 0., 0., 0., 0., 0.];
+            m = zeros(1,6);
+            sf_j0 = 0.;
+            tf_j0 = 0.;
         end
     end
 end
