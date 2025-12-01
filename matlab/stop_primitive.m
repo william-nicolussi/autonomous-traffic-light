44acea749802275c53fc02d00d131e0ac3c8997b
William Nicolussi Zom <william.nicolussi@gmail.com>
2025-12-01 19:39:10 +0100

    Stop Primitive OK

--- matlab/stop_primitive.m
+++ matlab/stop_primitive.m
@@ -26,7 +26,7 @@
 %% Stopping Primitive
 
 function [m, s_max, tf] = stop_primitive(v0, a0, sf)
-    if v0<=0 || sf==0  % If negative velocity or null final position, return all 0
+    if (v0<=0) || (sf==0)  % If negative velocity or null final position, return all 0
         s_max = 0.;
         tf = 0.;
         m = zeros(1, 6);
@@ -39,4 +39,5 @@ function [m, s_max, tf] = stop_primitive(v0, a0, sf)
             tf = final_opt_time_stop(v0, a0, s_max);
         end
         m = coef_list(v0, a0, s_max, 0, 0, tf);
+    end
 end
