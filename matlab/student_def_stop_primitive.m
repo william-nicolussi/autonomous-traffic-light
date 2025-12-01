a507f91403b31b43effe8ed9e71b31d51e45711c
William Nicolussi Zom <william.nicolussi@gmail.com>
2025-12-01 19:40:11 +0100

    Def Stop Primitive OK

--- matlab/student_def_stop_primitive.m
+++ matlab/student_def_stop_primitive.m
@@ -19,7 +19,7 @@ addpath(exportFolder);
 %% Determine the optimal time to stop %%
 syms z; % We create this variable for more stable solution of the equations
 syms v0 a0 sf tf;
-J = total_cost_var(v0, a0, sf, 0, 0, tf); %use function declared in student_def_optimal_control.m
+J = total_cost(v0, a0, sf, 0, 0, tf); %use function declared in student_def_optimal_control.m
 Jz = subs(J, tf, 1/z);
 dJz = diff(Jz, z);
 z_all = solve(dJz==0, z);
