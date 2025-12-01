fb78e9a39dc74d94a62a912433be59fa3eb105b5
William Nicolussi Zom <william.nicolussi@gmail.com>
2025-12-01 19:39:29 +0100

    Stop Primitive j0 OK

--- matlab/stop_primitive_j0.m
+++ matlab/stop_primitive_j0.m
@@ -6,14 +6,14 @@
 %
 %
 %%
-function [coeffs_j0, sf_j0, tf_j0] = student_stop_primitive_j0(v0, a0)
-    if v0>0 && a<0
+function [m, sf_j0, tf_j0] = stop_primitive_j0(v0, a0)
+    if (v0>0) && (a0<0)
         tf_j0 = final_opt_time_stop_j0(v0, a0);
         sf_j0 = final_opt_pos_j0(v0, a0, tf_j0);
-        coeffs_j0 = coef_list_fun(v0, a0, sf_j0, 0., 0., tf_j0);
+        m = coef_list(v0, a0, sf_j0, 0., 0., tf_j0);
     else
         tf_j0 = 0.;
         sf_j0 = 0.;
-        coeffs_j0 = [0., 0., 0., 0., 0., 0.];
+        m = zeros(1,6);
     end
 end
