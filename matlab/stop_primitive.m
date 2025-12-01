9c2876faa27ea3e884d732ffa377ec1838d78e02
William Nicolussi Zom <william.nicolussi@gmail.com>
2025-12-01 17:12:09 +0100

    Matlab Folder

--- matlab/stop_primitive.m
+++ matlab/stop_primitive.m
@@ -25,19 +25,18 @@
 
 %% Stopping Primitive
 
-function [coefs,maxsf,tf] = student_stop_primitive(v0,a0,sf)
+function [m, s_max, tf] = stop_primitive(v0, a0, sf)
     if v0<=0 || sf==0  % If negative velocity or null final position, return all 0
-        maxsf = 0;
+        s_max = 0.;
         tf = 0.;
-        coefs=[0,0,0,0,0,0];
-        return
-    end
-    if 4*v0^2+5*a0*sf<0  % --- Unfeasible case ---
-        maxsf = - (4*v0^2)/(5*a0);
-        tf = (10*maxsf)/(2*v0);
-    else  % --- Feasible case ---
-        maxsf = sf;
-        tf = final_opt_time_stop(v0, a0, maxsf);
-    end
-    coefs = coef_list(v0,a0,maxsf,0,0,tf);
+        m = zeros(1, 6);
+    else
+        if 4*v0^2+5*a0*sf < 0  % --- Unfeasible case ---
+            s_max = - (4*v0^2)/(5*a0);
+            tf = (10*s_max)/(2*v0);
+        else  % --- Feasible case ---
+            s_max = sf;
+            tf = final_opt_time_stop(v0, a0, s_max);
+        end
+        m = coef_list(v0, a0, s_max, 0, 0, tf);
 end
